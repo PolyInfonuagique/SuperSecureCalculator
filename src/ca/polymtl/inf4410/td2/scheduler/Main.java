8d136d2f02ed54cc84031d16355b7a1a351c4a4f
David Livet <david.livet@orange.fr>
2015-11-09 16:24:23 -0500

    Project init

---
+++ src/ca/polymtl/inf4410/td2/scheduler/Main.java
@@ -0,0 +1,8 @@
+package ca.polymtl.inf4410.td2.scheduler;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/ca/polymtl/inf4410/td2/server/Main.java
@@ -0,0 +1,8 @@
+package ca.polymtl.inf4410.td2.server;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
